8c50361b2c0dde5f98ca2c57ffe11b65dc83d007
Kesh Kshetrapalapuram <kaesava@gmail.com>
2015-12-01 10:29:09 +1100

    backend rails clean-up and Aptana workspace

---
+++ backend/rails/aptana_workspace/.metadata/.plugins/com.aptana.ruby.core/-388605728/4/threadgroup.rb
@@ -0,0 +1,24 @@
+class ThreadGroup < Object
+
+  Default = #<ThreadGroup:0x007ff50b89fe38>
+
+
+  def add(arg0)
+  end
+
+  def enclose
+  end
+
+  def enclosed?
+  end
+
+  def list
+  end
+
+
+  protected
+
+
+  private
+
+end
